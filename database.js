8a158d0c4d89a0bc20c417c1fa5fc2e4a37d8514
Jeremy <136492588+jeremylewis17@users.noreply.github.com>
2023-08-23 12:54:40 -0700

    Set up .env file

--- database.js
+++ database.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    //STILL NEED TO CONFIGURE MY DATABASE HERE
-    user: 'your_username',
-    host: 'your_host',
-    database: 'your_database',
-    password: 'your_password',
-    port: 5432
+    //STILL NEED TO CONFIGURE MY DATABASE IN .ENV
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 module.exports = { pool };
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const session = require('express-session');
 const passport = require('./passport'); // Import the Passport configuration from passport.js
 const routes = require('./routes'); // Import the server routes from routes.js
+
 
 const app = express();
 const PORT = process.env.PORT || 4001;
 
 app.use(
     session({
-        secret: "secret-key",
+        secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
         cookie: { maxAge: 300000000, secure: true },
