89a1c49a5dd181d530e5e74967eb561b20152977
Jeremy <136492588+jeremylewis17@users.noreply.github.com>
2023-08-23 13:20:21 -0700

    Add username/password validation

--- routes.js
+++ routes.js
@@ -17,16 +17,39 @@ router.post('/register', async (req, res) => {
     const { username, password } = req.body;
 
     try {
+        // Validation
+        if (!username || !password) {
+            return res.status(400).send('Username and password are required.');
+        }
+
+        // Check if the username is within the desired length range
+        if (username.length < 4 || username.length > 20) {
+            return res.status(400).send('Username must be between 4 and 20 characters.');
+        }
+
+        // Check if the password is within the desired length range
+        if (password.length < 8 || password.length > 30) {
+            return res.status(400).send('Password must be between 8 and 30 characters.');
+        }
+
+        // Check if the username already exists
+        const existingUser = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+        if (existingUser.rows.length > 0) {
+            return res.status(409).send('Username already exists.');
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
 
         // Insert the user into the PostgreSQL database
         await pool.query('INSERT INTO users (username, hashed_password) VALUES ($1, $2)', [username, hashedPassword]);
 
-        res.redirect('/login');
+        // Provide feedback to the user
+        res.status(201).send('Registration successful. You can now log in.');
     } catch (err) {
         console.error('Error creating user:', err);
-        res.status(500).send('Error creating user');
+        res.status(500).send('An error occurred during registration.');
     }
 });
+
 
 module.exports = router;
