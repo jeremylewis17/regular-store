0f9eacb31009006ae13e7c33465c0ed60c9b63fe
Jeremy <136492588+jeremylewis17@users.noreply.github.com>
2023-08-25 08:22:28 -0700

    Fix pool import

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    //STILL NEED TO CONFIGURE MY DATABASE IN .ENV
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
--- users.js
+++ users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const pool = require('./database');  // Import the PostgreSQL pool from database.js
+const {pool} = require('./database');  // Import the PostgreSQL pool from database.js
 const {passport, ensureAuthenticated} = require('./passport'); // Import the Passport configuration from passport.js
 const bcrypt = require('bcrypt');
 
